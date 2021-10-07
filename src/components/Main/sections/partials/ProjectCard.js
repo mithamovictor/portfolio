5ac99c85abe7936499d8389e41b96257e6f0696a
victor <vickarungaru@gmail.com>
2021-10-07 10:50:21 +0300

    updated resume

--- src/components/Main/sections/partials/ProjectCard.js
+++ src/components/Main/sections/partials/ProjectCard.js
@@ -6,7 +6,7 @@ export default function ProjectCard(props) {
       <img src={props.src} alt={props.alt} className="project_image" width="100%" height="auto" />
       <div className="project_meta">
         <p>{props.title}</p>
-        <a href={props.url} target="_blank" rel="noopener noreferrer">Visit Site<i className="fas fa-external-link-alt"></i></a>
+        <a href={props.url}>Visit Site<i className="fas fa-external-link-alt"></i></a>
       </div>
     </div>
   )

a3746ccf8c2a629784327e55d009de06170b2a6e
victor <vickarungaru@gmail.com>
2021-11-07 22:47:16 +0300

    added academic prowess dashboard to the portfolio

--- src/components/Main/sections/partials/ProjectCard.js
+++ src/components/Main/sections/partials/ProjectCard.js
@@ -5,7 +5,7 @@ export default function ProjectCard(props) {
     <div className="project_card">
       <img src={props.src} alt={props.alt} className="project_image" width="100%" height="auto" />
       <div className="project_meta">
-        <p>{props.title}</p>
+        <p className="text-center">{props.title}</p>
         <a href={props.url}>Visit Site<i className="fas fa-external-link-alt"></i></a>
       </div>
     </div>

cf873bbbcaf767598ad44c44b46506a11eb87b8b
victor <vickarungaru@gmail.com>
2022-01-04 19:20:26 +0300

    updated hire me section contact buttons

--- src/components/Main/sections/Contact.js
+++ src/components/Main/sections/Contact.js
@@ -6,30 +6,30 @@ const Contact = () => {
     <section className="contact_section" id="contact">
       <h2 className="section_heading">Hire <span className="yellow">Me</span></h2>
       <div className="contact_details">
-        <div className="contact_box">
+        <a className="contact_box" href='mailto:vickarungaru@gmail.com'>
           <div className="contact_icon">
             <i className="fas fa-envelope yellow"></i>
           </div>
           <div className="contact">
             <p className="yellow">vickarungaru@gmail.com</p>
           </div>
-        </div>
-        <div className="contact_box">
+        </a>
+        <a className="contact_box" href='tel:+254722402438'>
           <div className="contact_icon">
             <i className="fas fa-phone-volume yellow"></i>
           </div>
           <div className="contact">
-            <p className="yellow">+254722402438</p>
+            <p className="yellow">+254 (0) 722 402 438</p>
           </div>
-        </div>
-        <div className="contact_box">
+        </a>
+        <a className="contact_box" href='skype:vickarungaru_2'>
           <div className="contact_icon">
             <i className="fab fa-skype yellow"></i>
           </div>
           <div className="contact">
             <p className="yellow">vickarungaru_2</p>
           </div>
-        </div>
+        </a>
       </div>
       <ContactForm />
     </section>

9b7059c7199841b3a304dcfeb443ae9f472d187e
victor <vickarungaru@gmail.com>
2022-05-23 10:02:02 +0300

    initial setup version 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import React from 'react'
-
-import OffCanvas from './components/OffCanvas/OffCanvas'
-import Header from './components/Header/Header'
-import Main from './components/Main/Main'
-import Footer from './components/Footer/Footer'
-
 const App = () => {
-  return (
-    <>
-      <OffCanvas />
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  )
-}
+  return <h1>Hello World!</h1>;
+};
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class", // or 'media' or 'class' or false
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      altpurple: "#440055",
+    }),
+    textColor: (font) => ({
+      ...font("colors"),
+    }),
+    borderColor: (border) => ({
+      ...border("colors"),
+    }),
+    maxWidth: {
+      500: "500px",
+      80: "80px",
+    },
+    minWidth: {
+      500: "500px",
+      80: "80px",
+    },
+    extend: {
+      zIndex: {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+      },
+      transitionProperty: {
+        width: "width",
+      },
+    },
+  },
+  corePlugins: {
+    maxHeight: true,
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["even", "odd"],
+      backgroundOpacity: ["even", "odd"],
+      borderColor: ["hover", "focus", "active"],
+    },
+  },
+};

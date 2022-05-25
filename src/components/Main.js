db4873e42bed1dfc1c99f48baacf3bb78ddd05a6
victor <vickarungaru@gmail.com>
2022-05-25 23:25:34 +0300

    fixed scroll bug

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,8 @@ import Skills from "./Sections/Skills";
 
 const Main = () => {
   return (
-    <div className="flex flex-col justify-between md:max-h-screen max-w-screen-lg bg-green overflow-none md:overflow-scroll w-full h-full grow-1 mx-auto">
-      <div className="flex flex-col p-4 lg:p-16 grow-1">
+    <div className="flex flex-col justify-between md:max-h-screen bg-green overflow-none md:overflow-scroll w-full h-full grow-1">
+      <div className="flex flex-col p-4 lg:p-16 grow-1 mx-auto max-w-screen-lg">
         <About />
         <Skills />
         <Portfolio />

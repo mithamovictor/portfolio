0189eba33bab72b7ddf67d8cd575c89d7d6380dc
victor <vickarungaru@gmail.com>
2022-05-26 19:27:59 +0300

    added heading to contact form

--- src/components/Sections/Contact.js
+++ src/components/Sections/Contact.js
@@ -8,6 +8,9 @@ const Contact = () => {
       className="flex flex-col justify-center items-center my-8"
     >
       <SectionTitle title="Contact" />
+      <h3 className="text-2xl text-chocolate dark:text-cream">
+        Hire me for your next project
+      </h3>
       <ContactForm />
     </section>
   );

de613de2ea57df912aae9ff024523ea5a9f35a17
Victor Mithamo <vickarungaru@gmail.com>
2024-12-25 13:13:08 +0300

    finalized missing pages

--- src/pages/public-pages/blog.tsx
+++ src/pages/public-pages/blog.tsx
@@ -1,13 +1,13 @@
 import { FC } from "react";
 import Lottie from "lottie-react";
-import comingSoon from '../../assets/lottie/cs.json'
+import comingSoon from "../../assets/lottie/cs.json";
 
 const Blogs: FC = () => {
   return (
     <div className="flex flex-col w-full justify-center items-center">
       <h1 className="text-xl md:text-2xl">Blogs</h1>
 
-      <Lottie className={'w-full max-w-[400px]'} animationData={comingSoon} />
+      <Lottie className={"w-full max-w-[400px]"} animationData={comingSoon} />
     </div>
   );
 };

bebb269092649138824ffc0bd2b21addfa37fe49
Victor Mithamo <vickarungaru@gmail.com>
2025-09-15 17:59:17 +0300

    reduced social links

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -5,9 +5,9 @@ import { FaGithub, FaGitlab } from 'react-icons/fa';
 
 const Socials: FC = () => (
   <>
-    <Link className={'text-2xl'} href={'https://x.com/mithamo_vic'} target={'_blank'} rel={'noopener noreferrer'}>
-      <FaXTwitter />
-    </Link>
+    {/*<Link className={'text-2xl'} href={'https://x.com/mithamo_vic'} target={'_blank'} rel={'noopener noreferrer'}>*/}
+    {/*  <FaXTwitter />*/}
+    {/*</Link>*/}
     <Link
       className={'text-2xl'}
       href={'https://github.com/mithamovictor'}
@@ -16,17 +16,17 @@ const Socials: FC = () => (
     >
       <FaGithub />
     </Link>
+    {/*<Link*/}
+    {/*  className={'text-2xl'}*/}
+    {/*  href={'https://gitlab.com/victormithamo'}*/}
+    {/*  target={'_blank'}*/}
+    {/*  rel={'noopener noreferrer'}*/}
+    {/*>*/}
+    {/*  <FaGitlab />*/}
+    {/*</Link>*/}
     <Link
       className={'text-2xl'}
-      href={'https://gitlab.com/victormithamo'}
-      target={'_blank'}
-      rel={'noopener noreferrer'}
-    >
-      <FaGitlab />
-    </Link>
-    <Link
-      className={'text-2xl'}
-      href={'https://www.linkedin.com/in/victor-mithamo-030646b1/'}
+      href={'https://www.linkedin.com/in/mithamovictor/'}
       target={'_blank'}
       rel={'noopener noreferrer'}
     >

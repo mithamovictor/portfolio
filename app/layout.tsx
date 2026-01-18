da1d41c315a4f3917913ee93321b48f55cb6e516
Victor Mithamo <vickarungaru@gmail.com>
2026-01-19 00:33:08 +0300

    fix: remove unused dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './globals.css';
-import 'animate.css';
-import 'react-toastify/dist/ReactToastify.css';
 import Header from '@/app/components/Header';
 import Footer from '@/app/components/Footer';
 
